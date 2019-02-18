3b63f8ba526cc6c0d2ffdc62ca1c3e784bdf6325
Leo Brescia <leonardo@leobrescia.com.br>
2019-02-18 15:09:01 -0300

    feat: WIP header

--- wp-content/themes/tool-box/header.php
+++ wp-content/themes/tool-box/header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -13,46 +14,28 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+  <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'tool-box' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$tool_box_description = get_bloginfo( 'description', 'display' );
-			if ( $tool_box_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $tool_box_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+  <div class="container">
+    <header id="masthead" class="site-header">
+      <div class="row">
+        <div class="col-12 logo">
+          <a href="<?php echo esc_url(home_url('/')); ?>" rel="home" title="<?php bloginfo( 'name' ); ?>">
+            <img class="logo__img" src="<?= get_stylesheet_directory_uri(); ?>/images/tool-box.png" alt="<?php bloginfo( 'name' ); ?>">
+          </a>
+        </div>
+        <div class=" col - 12 ">
+        </div>
+      </div>
+    </header><!-- #masthead -->
+  </div>
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'tool-box' ); ?></button>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
 
-	<div id="content" class="site-content">
