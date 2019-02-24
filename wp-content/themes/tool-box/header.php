03b052bee0eeeb6552b0d6c374f01a21cc6ee387
Leo Brescia <leonardo@leobrescia.com.br>
2019-02-24 11:14:07 -0300

    feat: Add site header

--- wp-content/themes/tool-box/header.php
+++ wp-content/themes/tool-box/header.php
@@ -12,30 +12,95 @@
 
 ?>
 <!doctype html>
-<html <?php language_attributes(); ?>>
+<!--[if IE 7]>
+<html class="ie ie7 no-js" <?php language_attributes(); ?> >
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8 no-js" <?php language_attributes(); ?> >
+<![endif]-->
+<!--[if !(IE 7) & !(IE 8)]><!-->
+<html class="no-js" <?php language_attributes(); ?>>
+
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="profile" href="https://gmpg.org/xfn/11">
 
   <?php wp_head(); ?>
+  <!--[if lt IE 9]>
+    <script src="https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
+  <div id="page" class="site">
 
-  <div class="container">
-    <header id="masthead" class="site-header">
-      <div class="row">
-        <div class="col-12 logo">
-          <a href="<?php echo esc_url(home_url('/')); ?>" rel="home" title="<?php bloginfo( 'name' ); ?>">
-            <img class="logo__img" src="<?= get_stylesheet_directory_uri(); ?>/images/tool-box.png" alt="<?php bloginfo( 'name' ); ?>">
-          </a>
+    <div class="container">
+      <header id="masthead" class="site-header">
+        <div class="row top top--desktop">
+
+          <div class="top-icons">
+            <ul class="top-icons__social">
+              <li>
+                <a href="http://" target="_blank" rel="noopener noreferrer">
+                  <img class="logo__img"
+                    src="<?= get_stylesheet_directory_uri(); ?>/images/ic-face.svg"
+                    onmouseover="this.src='<?= get_stylesheet_directory_uri(); ?>/images/ic-face-hover.svg'"
+                    onmouseout="this.src='<?= get_stylesheet_directory_uri(); ?>/images/ic-face.svg'"
+                    alt="<?php bloginfo('name'); ?> - Facebook">
+                </a>
+              </li>
+              <li>
+                <a href="http://" target="_blank" rel="noopener noreferrer">
+                  <img class="logo__img"
+                    src="<?= get_stylesheet_directory_uri(); ?>/images/ic-instagram.svg"
+                    onmouseover="this.src='<?= get_stylesheet_directory_uri(); ?>/images/ic-instagram-hover.svg'"
+                    onmouseout="this.src='<?= get_stylesheet_directory_uri(); ?>/images/ic-instagram.svg'"
+                    alt="<?php bloginfo('name'); ?> - Instagram">
+                </a>
+              </li>
+              <li>
+                <a href="http://" target="_blank" rel="noopener noreferrer">
+                  <img class="logo__img"
+                    src="<?= get_stylesheet_directory_uri(); ?>/images/ic-youtube.svg"
+                    onmouseover="this.src='<?= get_stylesheet_directory_uri(); ?>/images/ic-youtube-hover.svg'"
+                    onmouseout="this.src='<?= get_stylesheet_directory_uri(); ?>/images/ic-youtube.svg'"
+                    alt="<?php bloginfo('name'); ?> - Youtube">
+                </a>
+              </li>
+              <li>
+                <a href="http://" target="_blank" rel="noopener noreferrer">
+                  <img class="logo__img"
+                    src="<?= get_stylesheet_directory_uri(); ?>/images/ic-pinterest.svg"
+                    onmouseover="this.src='<?= get_stylesheet_directory_uri(); ?>/images/ic-pinterest-hover.svg'"
+                    onmouseout="this.src='<?= get_stylesheet_directory_uri(); ?>/images/ic-pinterest.svg'"
+                    alt="<?php bloginfo('name'); ?> - Pinterest">
+                </a>
+              </li>
+            </ul>
+            <div class="top-icons__links">
+              <a href="">Quem somos</a>
+              <span>/</span>
+              <a href="">Contato</a>
+            </div>
+
+          </div>
+          <div class="col-12 logo">
+            <a href="<?php echo esc_url(home_url('/')); ?>" rel="home" title="<?php bloginfo('name'); ?>">
+              <img class="logo__img" src="<?= get_stylesheet_directory_uri(); ?>/images/tool-box.png"
+                alt="<?php bloginfo('name'); ?>">
+            </a>
+          </div>
+
+          <div class="col-12 menu">
+            <?php
+            tool_box_custom_menu('menu-1');
+            get_search_form();
+            ?>
+
+          </div>
+
         </div>
-        <div class=" col - 12 ">
-        </div>
-      </div>
-    </header><!-- #masthead -->
-  </div>
-
-
+      </header><!-- #masthead -->
+    </div>
